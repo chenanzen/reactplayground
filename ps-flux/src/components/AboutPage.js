90b8f38ad20f720699f103284f25631f135809e7
Ali Susanto <chenanzen@yahoo.com>
2025-02-26 19:52:51 +0800

    completed module 5

---
+++ ps-flux/src/components/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AboutPage extends React.Component{
+    render(){
+        return (
+            <>
+                <h2>About</h2>
+                <p>This application uses React. test </p>
+            </>
+        );
+    }
+}
+
+export default AboutPage;
---
+++ ps-flux/src/components/App.js
@@ -0,0 +1,22 @@
+import React  from "react";
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+import Header from "./common/Header";
+
+function App(){
+
+    function getpage(){
+        const route = window.location.pathname;
+        if(route === '/about') return <AboutPage />;
+        return <HomePage />;
+    }
+
+    return (
+        <div className='container-fluid'>
+            <Header />
+            {getpage()}
+        </div>
+    );
+}
+
+export default App;
